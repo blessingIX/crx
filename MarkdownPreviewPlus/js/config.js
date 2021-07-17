6d38b6f3331bee14319ae43f05579e9744afcc10
blessingIX <1350437095@qq.com>
2021-07-17 10:04:19 +0800

    add crx: MarkdownPreviewPlus, WordDelimitationTranslation.

---
+++ MarkdownPreviewPlus/js/config.js
@@ -0,0 +1,19 @@
+window.config = (function(hljs) {
+    // Define module
+    var module = {};
+
+    // Public module properties
+    module.markedOptions = {
+        gfm: true,
+        tables: true,
+        breaks: false,
+        sanitize: true,
+        katex: false,
+        highlight: function(code, language) {
+            const languageSubset = language ? [language]: null;
+            return hljs.highlightAuto(code, languageSubset).value;
+        }
+    };
+
+    return module;
+}(hljs));
---
+++ MarkdownPreviewPlus/test/test_sequence.markdown
@@ -0,0 +1,8 @@
+# Test Seqence
+```sequence
+Title: Here is a title
+A->B: Normal link
+B-->C: Dashed line
+C->>D: Open arrow
+D-->>A: dotted line open arrow
+```
